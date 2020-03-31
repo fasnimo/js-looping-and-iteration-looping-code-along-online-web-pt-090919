3e12bd9d1a9d703c37730981e94a4d14926dc623
fasnimo <fasnimo@gmail.com>
2020-03-31 01:16:11 -0500

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Code your solutions in this file
+
+function writeCards(names, event)  {
+    let card = []
+    for (let i = 0; i < names.length; i++) {
+    card.push(`Thank you, ${names[i]}, for the wonderful ${event} gift!`);
+    }
+    return card;
+}
+
+function countDown(int) {
+    while (int >= 0) {
+        console.log(int);
+        int--
+    }
+    return int;
+}
