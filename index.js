438b317b280fcf60e94c9e34431354681a9cc57b
fasnimo <fasnimo@gmail.com>
2020-03-31 01:19:39 -0500

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-// Code your solutions in this file
 
+// arr[ 'Lisa', 'Kaitlin', 'Jan' ], 'surprise' )
 function writeCards(names, event)  {
     let card = []
     for (let i = 0; i < names.length; i++) {
@@ -7,7 +7,7 @@ function writeCards(names, event)  {
     }
     return card;
 }
-
+// "Expected countDown(10) to invoke 11 console.logs"
 function countDown(int) {
     while (int >= 0) {
         console.log(int);
